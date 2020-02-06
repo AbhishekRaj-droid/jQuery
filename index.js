5bda2acd5c84258ecfa1498ef093ce3856928f2e
Abhishek Raj <deepclearning@gmail.com>
2020-02-06 16:44:01 +0530

    jquery added

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-$('h1').css("color", "red");
+// $('h1').css("color", "red");
+
+$('h1').addClass("big-title margin-10")
+
+//button javascript
+
+$('button').addClass("margin-10")
+
+//manipute text 
+
+$('button').html("<em>Hey</em>")
+$('button').text("Hey via text method")
+
+// manipulate attribute
+
+//eventlistener
+$('h1').click(function() {
+    $('h1').css("color", "purple");
+})
